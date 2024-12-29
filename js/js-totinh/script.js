372de3cb91222389e2f7970249f9c9a1f531b83c
Kiiro Ryu <89684135+hoanglongryu@users.noreply.github.com>
2024-12-29 09:26:57 +0700

    Add files via upload

---
+++ js/js-totinh/script.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao cọu cứ đi nhầm đường
+    Lạc vào tim tớ lẻ loi
+    -Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Cọu,... có đồng ý làm ngừi iu tớ chứ 😙',
+    desc: 'Cọu mà thoát là có nghĩa cọu là vợ của tớ đóa ',
+    btnYes: 'Tớ đồng ý hihi :3',
+    btnNo: 'Hong, hong đựt đou :}',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cọu lại thích tớ <3',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Vì tớ đã say cọu từ cái nhìn đầu tiên :3                 ',
+    mess: 'Tớ biết mà 🥰. Yêu cọu nhiều nhiều 😘😘',
+    messDesc: 'Nhắn với tớ nha bé iu.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.messenger.com/t/kajangky/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
